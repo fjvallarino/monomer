9588d82ebdf7865b9ff43de88b0751bd06ba28d1
Francisco Vallarino <fjvallarino@gmail.com>
2021-01-19 20:09:12 -0300

    Complete split tests

--- test/unit/Monomer/Widgets/SplitSpec.hs
+++ test/unit/Monomer/Widgets/SplitSpec.hs
@@ -25,7 +25,7 @@ import Monomer.Widgets.Split
 import qualified Monomer.Lens as L
 
 data TestEvt
-  = SliderChanged Int
+  = SliderChanged Double
   | Button1
   | Button2
   deriving (Eq, Show)
@@ -37,11 +37,9 @@ newtype TestModel = TestModel {
 makeLensesWith abbreviatedFields ''TestModel
 
 spec :: Spec
-spec = fdescribe "Split" $ do
+spec = describe "Split" $ do
   handleEventMouseDragH
   handleEventMouseDragV
-  -- Test auto space assignment
-  -- Test events
   updateSizeReq
 
 handleEventMouseDragH :: Spec
@@ -53,12 +51,19 @@ handleEventMouseDragH = describe "handleEventMouseDragH" $ do
     model steps ^. sliderPos `shouldBe` (220 / 640)
     areas steps `shouldBe` Seq.fromList [Rect 0 0 218 480, Rect 223 0 417 480]
 
-  it "should drag right 100 pixels, moving the slider" $ do
+  it "should drag left 200 pixels, but move the slider only 120" $ do
+    let selStart = Point 320 240
+    let selEnd = Point 120 100
+    let steps = evtDrag selStart selEnd
+    model steps ^. sliderPos `shouldBe` (200 / 640)
+    areas steps `shouldBe` Seq.fromList [Rect 0 0 198 480, Rect 203 0 437 480]
+
+  it "should drag right 100 pixels, but move the slider only 80" $ do
     let selStart = Point 320 240
     let selEnd = Point 420 100
     let steps = evtDrag selStart selEnd
-    model steps ^. sliderPos `shouldBe` (420 / 640)
-    areas steps `shouldBe` Seq.fromList [Rect 0 0 417 480, Rect 422 0 218 480]
+    model steps ^. sliderPos `shouldBe` (400 / 640)
+    areas steps `shouldBe` Seq.fromList [Rect 0 0 397 480, Rect 402 0 238 480]
 
   it "should drag up 100 pixels, keeping the slider intact" $ do
     let selStart = Point 320 240
@@ -76,10 +81,9 @@ handleEventMouseDragH = describe "handleEventMouseDragH" $ do
 
   where
     wenv = mockWenv (TestModel 0.5)
-    btn1 = button "Text" Button1
+    btn1 = button "Text" Button1 `style` [rangeWidth 200 400]
     btn2 = button "Longer" Button2
     splitNode = hsplit_ (btn1, btn2) [splitHandlePos sliderPos]
-    evts es = nodeHandleEventEvts wenv es splitNode
     model es = nodeHandleEventModel wenv es splitNode
     areas es = ra where
       root = nodeHandleEventRoot wenv es splitNode
@@ -91,38 +95,44 @@ handleEventMouseDragV = describe "handleEventMouseDragV" $ do
     let selStart = Point 320 240
     let selEnd = Point 320 140
     let steps = evtDrag selStart selEnd
-    model steps ^. sliderPos `shouldBe` (140 / 480)
+    evts steps `shouldBe` Seq.fromList [SliderChanged (140 / 480)]
     areas steps `shouldBe` Seq.fromList [Rect 0 0 640 139, Rect 0 144 640 336]
 
-  it "should drag down 100 pixels, moving the slider" $ do
+  it "should drag up 200 pixels, but move the slider only 100" $ do
+    let selStart = Point 320 240
+    let selEnd = Point 320 40
+    let steps = evtDrag selStart selEnd
+    evts steps `shouldBe` Seq.fromList [SliderChanged (80 / 480)]
+    areas steps `shouldBe` Seq.fromList [Rect 0 0 640 79, Rect 0 84 640 396]
+
+  it "should drag down 100 pixels, but move the slider only 40" $ do
     let selStart = Point 320 240
     let selEnd = Point 320 340
     let steps = evtDrag selStart selEnd
-    model steps ^. sliderPos `shouldBe` (340 / 480)
-    areas steps `shouldBe` Seq.fromList [Rect 0 0 640 336, Rect 0 341 640 139]
+    evts steps `shouldBe` Seq.fromList [SliderChanged (280 / 480)]
+    areas steps `shouldBe` Seq.fromList [Rect 0 0 640 277, Rect 0 282 640 198]
 
   it "should drag left 100 pixels, keeping the slider intact" $ do
     let selStart = Point 320 240
     let selEnd = Point 220 240
     let steps = evtDrag selStart selEnd
-    model steps ^. sliderPos `shouldBe` 0.5
+    evts steps `shouldBe` Empty
     areas steps `shouldBe` Seq.fromList [Rect 0 0 640 238, Rect 0 242 640 238]
 
   it "should drag right 100 pixels, keeping the slider intact" $ do
     let selStart = Point 320 240
     let selEnd = Point 420 240
     let steps = evtDrag selStart selEnd
-    model steps ^. sliderPos `shouldBe` 0.5
+    evts steps `shouldBe` Empty
     areas steps `shouldBe` Seq.fromList [Rect 0 0 640 238, Rect 0 242 640 238]
 
   where
     wenv = mockWenv (TestModel 0.5)
     -- Uses flexHeight otherwise buttons are fixed size and split will not move
     btn1 = button "Text" Button1 `style` [flexHeight 20]
-    btn2 = button "Longer" Button2 `style` [flexHeight 20]
-    splitNode = vsplit_ (btn1, btn2) [splitHandlePos sliderPos]
+    btn2 = button "Longer" Button2 `style` [rangeHeight 200 400]
+    splitNode = vsplit_ (btn1, btn2) [splitHandlePosV 0.5, onChange SliderChanged]
     evts es = nodeHandleEventEvts wenv es splitNode
-    model es = nodeHandleEventModel wenv es splitNode
     areas es = ra where
       root = nodeHandleEventRoot wenv es splitNode
       ra = fmap (roundRectUnits . _wniRenderArea . _wnInfo) (root ^. L.children)
