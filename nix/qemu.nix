12960c8245267a4a1a4e2a878e752f628daf0e71
Providence Salumu <Providence.Salumu@smunix.com>
2021-08-20 22:39:24 -0400

    qemu vm: enable X11 windowing system

--- nix/qemu.nix
+++ nix/qemu.nix
@@ -73,6 +73,22 @@ in rec {
               wheelNeedsPassword = false;
             };
           };
+ 	  # Enable the X11 windowing system.
+	  services.xserver.enable = true;
+ 
+	  # Enable the KDE Desktop Environment.
+	  services.xserver.displayManager.sddm.enable = true;
+	  services.xserver.desktopManager.plasma5.enable = true;
+	  services.xserver = {
+	    windowManager = {
+	      awesome = {
+	        enable = true;
+	        luaModules = [ luaPackages.luaposix ];
+	      };
+	    };
+	  };
+	  services.xserver.autorun = true;
+
           virtualisation = {
             graphics = true;
             cores = 4;
