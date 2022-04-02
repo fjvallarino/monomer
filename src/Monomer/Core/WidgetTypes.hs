e49a79357653f171efe4a2e93a273ac9504d029f
Francisco Vallarino <fjvallarino@users.noreply.github.com>
2022-04-02 19:04:53 +0200

    Add missing instances to Millisecond type (#109)
    * Add Generic instance to Millisecond type
    
    * Add Read instance to Millisecond type
    
    * Add Default instance. Use newtype deriving whenever possible

--- src/Monomer/Core/WidgetTypes.hs
+++ src/Monomer/Core/WidgetTypes.hs
@@ -10,6 +10,7 @@ Basic types and definitions for Widgets.
 -}
 {-# LANGUAGE ConstraintKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE ExistentialQuantification #-}
 {-# Language GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE RankNTypes #-}
@@ -46,7 +47,8 @@ It can be converted from/to other numeric types using the standard functions.
 -}
 newtype Millisecond = Millisecond {
   unMilliseconds :: Word64
-} deriving (Show, Eq, Ord, Num, Enum, Bounded, Real, Integral, TextShow)
+} deriving newtype (Eq, Ord, Enum, Bounded, Num, Real, Integral, Read, Show, Default, TextShow)
+  deriving (Generic)
 
 -- | Type constraints for a valid model
 type WidgetModel s = Typeable s
